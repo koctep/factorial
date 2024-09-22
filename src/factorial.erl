309e584fdbd9049325ffca2441635884cf99bd70
Ilya Ashchepkov <i.ashchepkov@velvetech.com>
2024-09-22 14:30:47 +0200

    Use tail recursion

--- src/factorial.erl
+++ src/factorial.erl
@@ -3,12 +3,15 @@
 -export([calc/1]).
 -export([measure/1]).
 
-calc(0) ->
-    1;
-calc(N) when is_integer(N) andalso N > 0 ->
-    N * calc(N - 1);
+calc(N) when is_integer(N) andalso N >= 0 ->
+    calc(N, 1);
 calc(_) ->
     {error, badarg}.
+
+calc(0, Acc) ->
+    Acc;
+calc(N, Acc) ->
+    calc(N - 1, Acc * N).
 
 measure(N) ->
   element(1, timer:tc(?MODULE, calc, [N])).
