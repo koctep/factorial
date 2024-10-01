df1c7c7abdcb0b90abed30176ad67ce0f6201deb
Ilya Ashchepkov <i.ashchepkov@velvetech.com>
2024-10-01 19:56:18 +0200

    Network-distributed calculation

--- src/delegate.erl
+++ src/delegate.erl
@@ -4,13 +4,19 @@
 
 calc(RespondTo, N) ->
     NumWorkers = erlang:system_info(schedulers_online),
-    Step = NumWorkers,
-    logger:notice("starting ~p workers", [NumWorkers]),
-    start_workers(RespondTo, NumWorkers, Step, N),
-    NumWorkers.
+    Nodes = application:get_env(kernel, nodes, [node()]),
+    Step = NumWorkers * length(Nodes),
+    start_workers(RespondTo, NumWorkers, Step, N, Nodes),
+    Step.
 
-start_workers(_RespondTo, _Step, 0, _Start) ->
+start_workers(_RespondTo, _NumWorkers, _Step, _N, []) ->
     ok;
-start_workers(RespondTo, Step, NumWorkers, Start) ->
-    spawn(worker, start, [RespondTo, Start, Step]),
-    start_workers(RespondTo, Step, NumWorkers - 1, Start - 1).
+start_workers(RespondTo, NumWorkers, Step, N, [Node | Nodes]) ->
+    start_workers_at_node(RespondTo, NumWorkers, Step, N, Node),
+    start_workers(RespondTo, NumWorkers, Step, N - NumWorkers, Nodes).
+
+start_workers_at_node(_RespondTo, _Step, 0, _Start, _Node) ->
+    ok;
+start_workers_at_node(RespondTo, Step, NumWorkers, Start, Node) ->
+    spawn(Node, worker, start, [RespondTo, Start, Step]),
+    start_workers_at_node(RespondTo, Step, NumWorkers - 1, Start - 1, Node).
